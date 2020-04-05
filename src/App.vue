4baf5777c7107ced876959c38ebe3517a6e0f801
replygirl <moon@moon.fyi>
2020-04-04 22:49:53 -0400

    add stylus

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,7 @@ export default defineComponent({
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='stylus'>
+#app
+  font-family 'IBM Plex Mono', monospace
 </style>
