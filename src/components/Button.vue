b4b4c1c951c7a756ef8fd9d1912fe37a307bb7bc
replygirl <moon@moon.fyi>
2020-04-04 23:53:54 -0400

    style Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -16,10 +16,16 @@ export default defineComponent({
 
 <style scoped lang='stylus'>
 button
-  border 1px solid v(color)
-  color v(color)
+  border 1px solid $c
+  color $c
   cursor pointer
   font-size inherit
   line-height inherit
   padding 10px
+  transition all 75ms ease-out
+
+  @media (hover: hover)
+    &:hover
+      transform translate($nd, $nd)
+      box-shadow $d $d $c
 </style>
