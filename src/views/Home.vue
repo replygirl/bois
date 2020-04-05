39db7e721767fa4bec4c61c699141943faee77a0
replygirl <moon@moon.fyi>
2020-04-05 04:18:15 -0400

    add home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <template lang='pug'>
-.hello
-  h1(v-text='msg || "hi"')
+Nav(v-bind='{ title, links }')
+main
+  p the bois are ui components
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { defineComponent } from 'vue'
+import { Nav } from '@/components'
 
 export default defineComponent({
-  props: {
-    msg: String
+  components: {
+    Nav
+  },
+  data() {
+    return {
+      title: { name: 'Bois', to: '/' },
+      links: [
+        { name: 'GitHub', href: 'https://github.com/replygirl/bois' }
+      ]
+    }
   }
 })
 </script>
+
+<style scoped lang='stylus'>
+main
+  padding $g4 $g2
+</style>
